16ca7c2c7245731132d815f14dcf49d542a9aad2
cowboycodr <kian.p.m.007@gmail.com>
2025-06-22 21:24:39 -0500

    update lsp

--- lua/mckenna/plugins/lsp.lua
+++ lua/mckenna/plugins/lsp.lua
@@ -1,4 +1,4 @@
-local servers = { "gopls", "lua_ls", "nixd", "rust_analyzer", "svelte", "ts_ls" }
+local servers = { "clangd", "gopls", "lua_ls", "nixd", "rust_analyzer", "svelte", "ts_ls" }
 
 return {
 	"neovim/nvim-lspconfig",
